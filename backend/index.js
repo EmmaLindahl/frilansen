c3d20ffd2991636d3380621ff95391ee754ec492
Emma Lindahl <emma.sofia.dahl@gmail.com>
2024-11-01 10:43:02 +0100

    added express code

---
+++ backend/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+port = process.env.PORT || 3000
+
+app.use(cors())
+app.get('/', (_request, response) => {
+    response.send({Hello: 'world!'})
+})
+
+app.listen(port,() => {console.log(`Redo på http://localhost:${port}`)})
