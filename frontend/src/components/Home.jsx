50d9ac5cdb0947999b73b9d89481e1edda247958
Jonathan Dragonlord <jonathan95andersson@gmail.com>
2024-11-13 17:57:02 +0100

    GDPR Info and conditional rendering

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -22,23 +22,27 @@ const Home = () => {
 
   return (
     <div className='home-container'>
-      <div className='welcome-section'>
-        <h1>
-          Välkommen till Frilansen!
-        </h1>
-        {token? <h2>Kolla in våra frilansare!</h2> : 
-          <h2>Logga in eller fortsätt</h2>   
-        }
-        
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eaque, excepturi modi ad autem voluptates ullam et non nihil saepe sint voluptatum consequuntur sunt tenetur magni. Modi nisi consectetur quod itaque?</p>
-        {!token && <button onMouseEnter={preloadSearch} className='login-button' onClick={openModal}>
-          Logga in
-        </button>}
-      </div>
-    
-      <LoginModal isOpen={isModalOpen} onRequestClose={closeModal} />
-
-      {showGDPR && <GDPRInfo onClose={GDPRclose} />}
+      {showGDPR ? ( 
+        <GDPRInfo onClose={GDPRclose} /> 
+      ) : (
+        <>
+        <div className='welcome-section'>
+          <h1>
+            Välkommen till Frilansen!
+          </h1>
+          {token? <h2>Kolla in våra frilansare!</h2> : 
+            <h2>Logga in eller fortsätt</h2>   
+          }
+          
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eaque, excepturi modi ad autem voluptates ullam et non nihil saepe sint voluptatum consequuntur sunt tenetur magni. Modi nisi consectetur quod itaque?</p>
+          {!token && <button onMouseEnter={preloadSearch} className='login-button' onClick={openModal}>
+            Logga in
+          </button>}
+        </div>
+      
+        <LoginModal isOpen={isModalOpen} onRequestClose={closeModal} />
+      </>
+      )}
     </div>
   )
 }
