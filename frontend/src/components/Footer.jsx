6eeb3a73772cad08f44b8daf290dd5bab27ce4dd
Jonathan Dragonlord <jonathan95andersson@gmail.com>
2024-11-14 17:11:01 +0100

    minor change, footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -2,7 +2,10 @@ import React from 'react';
 
 const Footer = () => <>
 <div>
-  <p>Footer: Nånting Fint att läsa här</p>
+  <p>Personuppgiftsansvarig: Jonathan Andersson 0704976046
+    Företrädare: Emma Lindahl 0707070707
+    Dataskyddsombud: Filip Johansson 0701234567
+  </p>
 </div></>
 
 export default Footer;
