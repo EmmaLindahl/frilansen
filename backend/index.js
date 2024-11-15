5528bdefedcd8a93da70f0a08bc172a97d842636
Emma Lindahl <emma.sofia.dahl@gmail.com>
2024-11-15 09:59:29 +0100

    bugfix

--- backend/index.js
+++ backend/index.js
@@ -24,11 +24,11 @@ port = process.env.PORT || 3000
 const authenticateToken = (req, res, next) => {
     const token = req.header('Authorization')?.split(' ')[1];
     if(!token){
-        return response.status(401).json({error: 'No token provided'})
+        return res.status(401).json({error: 'No token provided'})
     }
     jwt.verify(token, process.env.JWT_SECRET, (err, decoded) =>{
         if(err){
-            return response.status(403).json({error: 'Invalid token'})
+            return res.status(403).json({error: 'Invalid token'})
         }
         req.user = decoded;
         next()
